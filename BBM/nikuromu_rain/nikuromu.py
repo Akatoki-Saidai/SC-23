eecf159e84bec1dd59c3f2c03d317f24f9798614
mori-yuuma <yumayuma0329@gmail.com>
2025-02-17 13:04:28 +0900

    Update nikuromu.py

--- BBM/nikuromu_rain/nikuromu.py
+++ BBM/nikuromu_rain/nikuromu.py
@@ -1,1 +1,10 @@
-#指定したピンに電流を流すコード
+import time
+import RPi.GPIO as GPIO
+#使うpin番号
+pin = 16
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin,1)
+#休ませる時間
+time.sleep(3)
+GPIO.output(pin,0)
