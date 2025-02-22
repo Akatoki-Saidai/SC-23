e62229cbb5778a3ca0fa26520a52fe0dcee75b09
YT2515 <167419242+yt2515@users.noreply.github.com>
2025-02-22 23:04:10 +0900

    csv記録講座ファイル更新

--- program/csv/main.py
+++ program/csv/main.py
@@ -11,18 +11,19 @@ def main():
     #※1
     #第一引数に関してだが基本は下のやつから選んでほしい
     #先輩のコード見て自分が記録しようとしてることがなんて名前のタイプ使ってたかを確認すると良い
-    msg_types = ['time', 'file', 'func', 'line', 'serious_error', 'error', 'warning', 'msg', 'format_exception', 'phase', 'gnss_time', 'date', 'lat', 'lon', 'alt', 'alt_base_press', 'goal_lat', 'goal_lon', 'temp', 'press', 'camera_area', 'camera_order', 'camera_center_x', 'camera_center_y', 'camera_frame_size_x', 'camera_frame_size_y', 'motor_l', 'motor_r', 'goal_relative_x', 'goal_relative_y', 'goal_relative_angle_rad', 'goal_distance', 'accel_all_x', 'accel_all_y', 'accel_all_z', 'accel_line_x', 'accel_line_y', 'accel_line_z', 'mag_x', 'mag_y', 'mag_z', 'gyro_x', 'gyro_y', 'gyro_z', 'grav_x', 'grav_y', 'grav_z', 'euler_x', 'euler_y', 'euler_z', 'nmea']
+    msg_types = ['serious_error', 'error', 'warning', 'msg', 'format_exception', 'phase', 'gnss_time', 'date', 'lat', 'lon', 'alt', 'alt_base_press', 'goal_lat', 'goal_lon', 'temp', 'press', 'camera_area', 'camera_order', 'camera_center_x', 'camera_center_y', 'camera_frame_size_x', 'camera_frame_size_y', 'motor_l', 'motor_r', 'goal_relative_x', 'goal_relative_y', 'goal_relative_angle_rad', 'goal_distance', 'accel_all_x', 'accel_all_y', 'accel_all_z', 'accel_line_x', 'accel_line_y', 'accel_line_z', 'mag_x', 'mag_y', 'mag_z', 'gyro_x', 'gyro_y', 'gyro_z', 'grav_x', 'grav_y', 'grav_z', 'euler_x', 'euler_y', 'euler_z', 'nmea']
     #先輩のコードを見てるとたまに上の行のやつにない奴が第一引数に入ってることがある(具体例は後述※2)
     #そのときは連絡ください
 
-    #msg_typeを増やすこともできます
-    #その時も連絡ください
+    #とりあえず先輩が使ってたmsg_typeをそのまま書いてるだけなのでmsg_typeを増やすこともできます
+    #増やしたい時は連絡ください
 
     #以下書き方の具体例
     make_csv.print("msg","データタイプ「msg」は最も基本的なメッセージを記録したいとき使います")
     make_csv.print("msg","9軸セットアップ完了")
     make_csv.print("msg","上の行みたいにつかいましょう")
 
+    make_csv.print("warning","警告メッセージを記録すんのに使う")
     make_csv.print("error","エラーメッセージを記録すんのに使う")
     make_csv.print("serious_error","深刻なエラーのとき使う，先輩はセンサーのセットアップ失敗したときに使ってた")
 
