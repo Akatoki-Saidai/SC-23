c0df5b19075f3fad3adff4a2e53e67f2ed9476a4
ponchi-creator <ponchi141421356@gmail.com>
2025-03-01 23:39:43 +0900

    Update drv8835.py

--- program/enkyori/drv8835.py
+++ program/enkyori/drv8835.py
@@ -505,7 +505,7 @@ def stop():
     time.sleep(1)
 
 
-print("retreat")
+"""print("retreat")
 retreat(motor_right,motor_left)
 time.sleep(2)
 stop()
@@ -524,3 +524,4 @@ leftturn(motor_right,motor_left)
 stop()
 
 print("Finish!!!!!!!!!!")
+"""
