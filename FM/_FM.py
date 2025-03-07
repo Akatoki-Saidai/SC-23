d388c01488ee93aa8bff7a04fca6d2222c79d7a3
ara-shun <aarraasshhuunn@gmail.com>
2025-03-08 03:22:17 +0900

    gpsの分形式から度形式に変換するコード追加

--- FM/_FM.py
+++ FM/_FM.py
@@ -67,7 +67,6 @@ GPIO.setup(5, GPIO.OUT)
 def get_latitude():
     ser = serial.Serial('/dev/serial0', 9600, timeout=10)
     # print("緯度取得開始")
-    count_lat = 0
 
     while True:
         line = ser.readline().decode('utf-8').strip()
@@ -75,38 +74,25 @@ def get_latitude():
             parts = line.split(',')
             try:
                 if len(parts) >= 7 and int(parts[6]) > 0:
-                    latitude = float(parts[2]) / 100
+                    latitude_deg_min = float(parts[2]) / 100  # 度分形式で取得
+                    
+                    # 度数形式に変換
+                    degrees = int(latitude_deg_min)
+                    minutes = latitude_deg_min - degrees
+                    latitude = degrees + minutes / 0.6 
+                    
                     ser.close()
                     # print("緯度取得完了")
-                    make_csv.print("lat",latitude)
                     return latitude
                     break
-                    
-                count_lat += 1
-                if count_lat % 100 == 0:
-                    print("緯度取得失敗")
-                    make_csv.print("msg","failed to get latitude data")
-
             except:
-                count_lat += 1
-                if count_lat % 100 == 0:
-                    print("緯度取得失敗")
-                    make_csv.print("msg","failed to get latitude data")
-
                 pass  # 例外処理を追加しました。
-                
-        count_lat += 1
-        if count_lat % 100 == 0:
-            print("緯度取得失敗")
-            make_csv.print("msg","failed to get latitude data")
-
         time.sleep(0.01)
 
 """経度を取得する関数（値が取得できるまで無限ループ）"""
 def get_longitude():
     ser = serial.Serial('/dev/serial0', 9600, timeout=10)
     # print("経度取得開始")
-    count_lon = 0
 
     while True:
         line = ser.readline().decode('utf-8').strip()
@@ -114,28 +100,19 @@ def get_longitude():
             parts = line.split(',')
             try:
                 if len(parts) >= 7 and int(parts[6]) > 0:
-                    longitude = float(parts[4]) / 100
+                    longitude_deg_min = float(parts[4]) / 100 # 度分形式で取得
+                    
+                    # 度数形式に変換
+                    degrees = int(longitude_deg_min)
+                    minutes = longitude_deg_min - degrees
+                    longitude = degrees + minutes / 0.6
+                    
                     ser.close()
                     # print("経度取得完了")
-                    make_csv.print("lon",longitude)
                     return longitude
                     break
-                count_lon += 1
-                if count_lon % 100 == 0:
-                    print("経度取得失敗")
-                    make_csv.print("msg","failed to get longitude data")
-
             except:
-                count_lon += 1
-                if count_lon % 100 == 0:
-                    print("経度取得失敗")
-                    make_csv.print("msg","failed to get longitude data")
                 pass  # 例外処理を追加しました。
-        count_lon += 1
-        if count_lon % 100 == 0:
-            print("経度取得失敗")
-            make_csv.print("msg","failed to get longitude data")
-
         time.sleep(0.01)
 
 """現在地からゴールまでの距離と角度を計算する関数"""
